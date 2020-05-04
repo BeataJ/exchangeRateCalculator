179a8d12189f0bd113c8e313baeb0d5ed1c27ccc
beata <beata@emc2wave.com>
2020-05-03 17:48:29 -0700

    add rate

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function calculator() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  console.log(currency_one, currency_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+    });
 }
 
 // Event Listners
